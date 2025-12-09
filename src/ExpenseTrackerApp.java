c40f793483fd526df48dbfef9f0fd0e7d677798f
acreatorwithvision <fake49403@gmail.com>
2025-12-09 23:09:54 +0530

    feat(init): Initialize basic project structure and main application class

---
+++ src/ExpenseTrackerApp.java
@@ -0,0 +1,10 @@
+package com.expensetracker;
+
+public class ExpenseTrackerApp{
+	public static void main(String[] args){
+	System.out.println("---Simple Console-based expense tracker---");
+
+	System.out.println("Application Initialized Successfully");
+	}
+	
+}
